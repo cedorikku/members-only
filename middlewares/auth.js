79970f51ccec93050ec8b800f70cf942847c0809
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-25 22:43:09 +0800

    feat: create custom auth middleware's own module

---
+++ middlewares/auth.js
@@ -0,0 +1,10 @@
+export const preventUnauthorized = (req, res, next) => {
+    if (!req.user) return res.redirect('/account/login');
+    next();
+};
+
+
+export const preventAuthenticated = (req, res, next) => {
+    if (req.user) return res.redirect('/');
+    next();
+};
