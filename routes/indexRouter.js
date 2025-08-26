d2789cd2ea76546175a96eb317a9dc2ed669132b
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-26 19:34:47 +0800

    feat: prevent members from accessing /join

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,23 @@ import { preventUnauthorized } from '../middlewares/auth.js';
 
 const router = Router();
 
+// Prevent members from accessing the endpoinT `/join`
+const guestsOnly = (req, res, next) => {
+    if (req.user.role !== 'guest') return res.redirect('/');
+    next();
+};
+
 router.get('/', indexController.getIndex);
-router.get('/join', preventUnauthorized, indexController.enterSecretGet);
-router.post('/join', preventUnauthorized, indexController.enterSecretPost);
+router.get(
+    '/join',
+    preventUnauthorized,
+    guestsOnly,
+    indexController.enterSecretGet,
+);
+router.post(
+    '/join',
+    preventUnauthorized,
+    guestsOnly,
+    indexController.enterSecretPost,
+);
 export default router;
