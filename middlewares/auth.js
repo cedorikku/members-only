c235a8c7b79c8798ed5c346f28e3e25ae77c4963
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-28 15:05:58 +0800

    feat: write admin only middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,3 +7,11 @@ export const preventAuthenticated = (req, res, next) => {
     if (req.user) return res.redirect('/');
     next();
 };
+
+export const adminOnly = [
+    preventUnauthorized,
+    (req, res, next) => {
+        if (req.user.role !== 'admin') return res.status(404).render('404');
+        next();
+    },
+];
