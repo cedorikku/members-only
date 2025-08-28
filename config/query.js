5c0e670f757f119d65e6dee3c9fcc5b3069da06e
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-28 15:01:04 +0800

    feat: include posts' id in res.locals.posts context

--- config/query.js
+++ config/query.js
@@ -22,7 +22,7 @@ const getUserByUsername = async (username) => {
 const getAllPosts = async (order = 'desc') => {
     // TODO: Add pagination
     const query = `
-        SELECT p.title, p.content, p.created_at, u.username, u.firstname, u.lastname 
+        SELECT p.id, p.title, p.content, p.created_at, u.username, u.firstname, u.lastname 
         FROM posts p JOIN users u ON p.author_id = u.id
         ORDER BY p.created_at ${order}
     `;
